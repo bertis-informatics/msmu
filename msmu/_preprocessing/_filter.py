0162857315e177e14951951bb37a9fbc0a29a4af
Byeongchan Lee <byeongchan.lee@bertis.com>
2025-01-17 14:54:01 +0900

    feat: add decoy filter and specify column names

--- msmu/_preprocessing/_filter.py
+++ msmu/_preprocessing/_filter.py
@@ -5,34 +5,12 @@ import pandas as pd
 
 from ._calculate_precursor_purity import calculate_precursor_purity
 
-### THIS BLOCK SHOULD GO TO UTIL ###
-from typing import get_type_hints
 
-
-def check_type(func):
-    def wrapper(*args, **kwargs):
-        # Ensure no positional arguments are passed
-        if args:
-            raise TypeError("This function only accepts keyword arguments")
-
-        # Get type hints, excluding the return type
-        type_hints = get_type_hints(func)
-        type_hints.pop("return", None)
-
-        # Check for the correct number of arguments
-        if len(kwargs) != len(type_hints):
-            raise TypeError(f"Expected {len(type_hints)} arguments, got {len(kwargs)}")
-
-        for key, value in kwargs.items():
-            if key not in type_hints:
-                raise TypeError(f"Unexpected argument '{key}'")
-
-        return func(**kwargs)
-
-    return wrapper
-
-
-def _get_column(adata: AnnData, colname: str, key: str) -> str:
+def _get_column(
+    adata: AnnData,
+    colname: str,
+    key: str,
+) -> pd.Series:
     # Check if the column is in the var or varm
     if colname in adata.var.columns:
         return adata.var[colname]
@@ -42,7 +20,6 @@ def _get_column(adata: AnnData, colname: str, key: str) -> str:
         raise ValueError(f"{colname} not found in the data")
 
 
-@check_type
 def add_q_value_filter(
     mdata: MuData,
     level: str,
@@ -63,18 +40,18 @@ def add_q_value_filter(
     protein_q = _get_column(adata, "protein_q", "search_result")
 
     # Create filter result
-    filter_df = pd.DataFrame(columns=["spectrum_q", "peptide_q", "protein_q"])
-    filter_df["spectrum_q"] = spectrum_q < threshold["spectrum"]
-    filter_df["peptide_q"] = peptide_q < threshold["peptide"]
-    filter_df["protein_q"] = protein_q < threshold["protein"]
+    filter_df = pd.DataFrame(columns=["low_spectrum_q", "low_peptide_q", "low_protein_q"])
+    filter_df["low_spectrum_q"] = spectrum_q < threshold["spectrum"]
+    filter_df["low_peptide_q"] = peptide_q < threshold["peptide"]
+    filter_df["low_protein_q"] = protein_q < threshold["protein"]
 
     # Store filter result
     if "filter" not in adata.varm_keys():
         adata.varm["filter"] = filter_df
     else:
-        adata.varm["filter"]["spectrum_q"] = filter_df["spectrum_q"]
-        adata.varm["filter"]["peptide_q"] = filter_df["peptide_q"]
-        adata.varm["filter"]["protein_q"] = filter_df["protein_q"]
+        adata.varm["filter"]["low_spectrum_q"] = filter_df["low_spectrum_q"]
+        adata.varm["filter"]["low_peptide_q"] = filter_df["low_peptide_q"]
+        adata.varm["filter"]["low_protein_q"] = filter_df["low_protein_q"]
 
     # Store filter threshold
     if "filter" not in adata.uns_keys():
@@ -85,18 +62,43 @@ def add_q_value_filter(
     return mdata
 
 
-@check_type
 def add_decoy_filter(
     mdata: MuData,
     level: str,
+    decoy_prefix: str = "rev",
 ) -> MuData:
     mdata = mdata.copy()
     adata = mdata[level]
 
+    # Get protein columns
+    proteins = _get_column(adata, "proteins", "search_result")
+
+    # Remove decoy from proteins column
+    adata.var["proteins_no_decoy"] = proteins.astype(str).apply(lambda x: _remove_decoy(x, decoy_prefix))
+
+    # Create filter result
+    filter_df = pd.DataFrame(columns=["not_decoy"])
+    filter_df["not_decoy"] = adata.var["proteins_no_decoy"].notna()
+
+    # Store filter result
+    if "filter" not in adata.varm_keys():
+        adata.varm["filter"] = filter_df
+    else:
+        adata.varm["filter"]["not_decoy"] = filter_df["not_decoy"]
+
+    # Store filter prefix
+    if "filter" not in adata.uns_keys():
+        adata.uns["filter"] = {"decoy": decoy_prefix}
+    else:
+        adata.uns["filter"]["decoy"] = decoy_prefix
+
     return mdata
 
 
-@check_type
+def _remove_decoy(row: pd.Series[str], decoy_prefix: str) -> pd.Series:
+    return ";".join([x for x in row if not x.startswith(decoy_prefix)])
+
+
 def add_precursor_purity_filter(
     mdata: MuData,
     level: str,
@@ -120,20 +122,25 @@ def add_precursor_purity_filter(
         threshold = {"min": threshold[0], "max": threshold[1]}
 
     # Get precursor purity
-    adata.var["purity"] = calculate_precursor_purity(
-        adata=adata,
-        mzml_files=mzml_files,
-    )
+    if "purity" not in adata.var_keys():
+        adata.var["purity"] = calculate_precursor_purity(
+            adata=adata,
+            mzml_files=mzml_files,
+        )
 
     # Create filter result
-    filter_df = pd.DataFrame(columns=["purity"])
-    filter_df["purity"] = adata.var["purity"].between(threshold["min"], threshold["max"])
+    filter_df = pd.DataFrame(columns=["high_purity"])
+    filter_df["high_purity"] = adata.var["purity"].between(
+        left=threshold["min"],
+        right=threshold["max"],
+        inclusive="right",
+    )
 
     # Store filter result
     if "filter" not in adata.varm_keys():
         adata.varm["filter"] = filter_df
     else:
-        adata.varm["filter"]["purity"] = filter_df["purity"]
+        adata.varm["filter"]["high_purity"] = filter_df["high_purity"]
 
     # Store filter threshold
     if "filter" not in adata.uns_keys():
@@ -144,7 +151,6 @@ def add_precursor_purity_filter(
     return mdata
 
 
-@check_type
 def apply_filter(mdata: MuData) -> MuData:
     mdata = mdata.copy()
 
