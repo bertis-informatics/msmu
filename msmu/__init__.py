db7c3854bf3b3ee8768fda068c2cc4de0907df64
wook <hyungwook.choi@bertis.com>
2025-02-17 11:23:03 +0900

    fix: add read_h5mu in init

--- msmu/__init__.py
+++ msmu/__init__.py
@@ -5,7 +5,7 @@ from . import _preprocessing as pp
 from . import _plotting as pl
 from . import _tools as tl
 
-from ._read_write._readers import mask_obs, merge_mudata, read_sage
+from ._read_write._readers import mask_obs, merge_mudata, read_sage, read_h5mu
 
 __version__ = "0.1.0_dev"
 
