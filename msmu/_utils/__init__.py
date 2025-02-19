c76a75e3ff1a66563e34d279a5c371f3cb53e5bc
Byeongchan Lee <byeongchan.lee@bertis.com>
2025-02-19 14:43:23 +0900

    feat: add get_modality_dict function to retrieve modality data from MuData object

--- msmu/_utils/__init__.py
+++ msmu/_utils/__init__.py
@@ -1,5 +1,7 @@
 from .subset import subset
+from .utils import get_modality_dict
 
 __all__ = [
     "subset",
+    "get_modality_dict",
 ]
---
+++ msmu/_utils/utils.py
@@ -0,0 +1,24 @@
+import anndata as ad
+import mudata as md
+
+
+def get_modality_dict(
+    mdata: md.MuData,
+    level: str | None = None,
+    modality: str | None = None,
+) -> dict[str, ad.AnnData]:
+    """Get modality data from MuData object"""
+
+    if (level == None) & (modality == None):
+        level = "psm"
+
+    mod_dict: dict = dict()
+    if level != None:
+        for mod_name in mdata.mod_names:
+            if mdata[mod_name].uns["level"] == level:
+                mod_dict[mod_name] = mdata[mod_name].copy()
+
+    elif modality != None:
+        mod_dict[modality] = mdata[modality].copy()
+
+    return mod_dict
