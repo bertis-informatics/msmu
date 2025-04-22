2568dd3177c2ef72abd39007f92db5544c52f580
hyungwook.choi <hyungwook.choi@bertis.com>
2025-04-22 13:37:08 +0900

    feat: add get_label function in utils for get MS label from psm adatas

--- msmu/_utils/__init__.py
+++ msmu/_utils/__init__.py
@@ -1,9 +1,10 @@
 from .subset import subset
-from .utils import get_modality_dict, get_fasta_meta, map_fasta
+from .utils import get_modality_dict, get_fasta_meta, map_fasta, get_label
 
 __all__ = [
     "subset",
     "get_modality_dict",
     "get_fasta_meta",
     "map_fasta",
+    "get_label",
 ]
