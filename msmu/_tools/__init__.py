ead643c17ea2418087d5744e1723c9e12a19d22d
wook <hyungwook.choi@bertis.com>
2025-09-05 15:10:50 +0900

    fix: remove unused mzml import from analysis tools

--- msmu/_tools/__init__.py
+++ msmu/_tools/__init__.py
@@ -1,11 +1,9 @@
-from . import _mzml as mzml
 from ._dea import _dea as dea
 from ._pca import pca
 from ._umap import umap
 from ._precursor_purity import compute_precursor_purity
 
 __all__ = [
-    "mzml",
     "pca",
     "umap",
     "dea",
