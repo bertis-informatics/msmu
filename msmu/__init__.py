2a65ba446a99dc67ed47a8f65081decd38dc829d
Byeongchan Lee <byeongchan.lee@bertis.com>
2025-09-18 11:35:45 +0900

    fix: update version handling and add setuptools_scm configuration

--- msmu/__init__.py
+++ msmu/__init__.py
@@ -7,7 +7,12 @@ from . import _tools as tl
 from ._read_write._readers import merge_mudata, read_diann, read_h5mu, read_sage
 from ._utils import get_fasta_meta, get_label, map_fasta, subset, add_quant, split_tmt, rename_obs
 
-__version__ = "0.1.8"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = version = "0.0.0"
+else:
+    version = __version__
 
 mudata.set_options(pull_on_update=False)
 pl._set_templates()
