d8f92e5fdec23e8db0b812d1ed184d937d7207a3
wook <hyungwook.choi@bertis.com>
2025-09-25 17:54:57 +0900

    feat: add logging utility with LogLevel class and setup_logger function

--- msmu/__init__.py
+++ msmu/__init__.py
@@ -1,20 +1,30 @@
 import mudata
 
+import logging
+from .logging_utils import setup_logger, LogLevel
+
 from . import _plotting as pl
 from . import _preprocessing as pp
-from . import _read_write as rw
 from . import _tools as tl
-from ._read_write._readers import merge_mudata, read_diann, read_h5mu, read_sage
+from ._read_write._reader_utils import merge_mudata
+from ._read_write._reader_registry import read_h5mu, read_sage, read_diann, read_maxquant
+from . import _utils as utils
 from ._utils import get_fasta_meta, get_label, map_fasta, subset, add_quant, split_tmt, rename_obs
 
 __version__ = "0.1.8"
 
+logger = logging.getLogger("msmu")
+logger.setLevel(LogLevel.INFO)
+setup_logger(level=LogLevel.INFO)
+
 mudata.set_options(pull_on_update=False)
 pl._set_templates()
 
+del LogLevel, logging, mudata
+
 __all__ = [
-    "rw",
     "pp",
     "pl",
     "tl",
+    "utils"
 ]
