5aa0bf94fb063c1b8148aee3cea1dfc493cb304d
wook <hyungwook.choi@bertis.com>
2025-11-18 19:48:58 +0900

    refactor: streamline filter functions and remove unused filters for improved clarity

--- msmu/_preprocessing/__init__.py
+++ msmu/_preprocessing/__init__.py
@@ -1,28 +1,19 @@
 from ._compute_precursor_purity import compute_precursor_purity
-from ._filter import (
-    add_all_nan_filter,
-    add_precursor_purity_filter,
-    add_prefix_filter,
-    add_q_value_filter,
-    apply_filter,
-)
+from ._filter import add_filter, apply_filter
 from ._infer_protein import infer_protein
 from ._normalise._normalise import feature_scale, log2_transform, normalise, adjust_ptm_by_protein
-from ._summarise._summarise import to_peptide, to_protein, to_ptm_site
+from ._summarise._summarise import to_peptide, to_protein, to_ptm
 
 __all__ = [
-    "add_q_value_filter",
-    "add_precursor_purity_filter",
-    "add_prefix_filter",
-    "add_all_nan_filter",
+    "add_filter",
     "apply_filter",
     "compute_precursor_purity",
     "log2_transform",
     "normalise",
     "feature_scale",
-    "adjust_ptm_by_protein",
     "to_peptide",
     "to_protein",
-    "to_ptm_site",
+    "to_ptm",
     "infer_protein",
+    "adjust_ptm_by_protein",
 ]
--- msmu/_preprocessing/_filter.py
+++ msmu/_preprocessing/_filter.py
@@ -1,274 +1,134 @@
-from pathlib import Path
-import re
-
+from typing import Literal
 import pandas as pd
-from anndata import AnnData
 from mudata import MuData
 
-from .._utils import get_modality_dict, subset
-from .._utils import subset, get_modality_dict, uns_logger
-
-# def add_q_value_filter(
-#    mdata: MuData,
-#    modality: str,
-#    threshold: float | list[float] | dict[str, float],
-# ) -> MuData:
-#    mdata = mdata.copy()
-#    adata = mdata[modality]
-#
-#    # Format threshold
-#    if isinstance(threshold, (float, int)):
-#        threshold = {"spectrum": threshold, "peptide": threshold, "protein": threshold}
-#    elif isinstance(threshold, list):
-#        threshold = {
-#            "spectrum": threshold[0],
-#            "peptide": threshold[1],
-#            "protein": threshold[2],
-#        }
-#
-#    # Get q values
-#    spectrum_q = _get_column(adata, "spectrum_q", "search_result")
-#    peptide_q = _get_column(adata, "peptide_q", "search_result")
-#    protein_q = _get_column(adata, "protein_q", "search_result")
-#
-#    # Get filter result
-#    filter_result_spectrum = spectrum_q < threshold["spectrum"]
-#    filter_result_peptide = peptide_q < threshold["peptide"]
-#    filter_result_protein = protein_q < threshold["protein"]
-#
-#    # Save filter
-#    _save_filter(
-#        adata, "q_value_spectrum", filter_result_spectrum, threshold["spectrum"]
-#    )
-#    _save_filter(adata, "q_value_peptide", filter_result_peptide, threshold["peptide"])
-#    _save_filter(adata, "q_value_protein", filter_result_protein, threshold["protein"])
-#
-#    return mdata
+from .._utils import subset, uns_logger
+from .._read_write._mdata_status import MuDataStatus
 
 
 @uns_logger
-def add_q_value_filter(
-    mdata: MuData,
-    threshold: float | list[float] | dict[str, float],
-) -> MuData:
+def add_filter(
+        mdata: MuData,
+        modality: str,
+        column: str,
+        keep: Literal[
+            "eq", "ne", "lt", "le", "gt", "ge", "contains", "not_contains"
+            ],
+        value: str | float | None
+    ) -> MuData:
+    """
+    Adds a filter to the specified modality in the MuData object based on the given condition.
+
+    Parameters:
+        mdata (MuData): MuData object to which the filter will be added.
+        modality (str): The modality within the MuData object to which the filter will be applied
+        column (str): The column in the modality's var DataFrame to apply the filter on.
+        keep (Literal["eq", "ne", "lt", "le", "gt",
+                            "ge", "contains", "not_contains"]): The condition to apply for filtering.
+        value (str | float | None): The value to compare against for filtering.
+
+    Returns:
+        MuData: MuData object with the added filter.
+    """
+
     mdata = mdata.copy()
-    adata = mdata["feature"]
+    mstatus = MuDataStatus(mdata)
 
-    q_val_categories = ["spectrum_q", "peptide_q", "protein_q"]
-    var_cols = adata.var.columns
-    q_val_cols = [x for x in var_cols if x in q_val_categories]
+    filter_name = f"{column}_{keep}_{value}"
 
-    # Format threshold
-    if isinstance(threshold, (float, int)):
-        threshold_dict = {q_col: threshold for q_col in q_val_cols}
-    elif isinstance(threshold, list):
-        threshold_dict = {q_col: threshold[i] for i, q_col in enumerate(q_val_cols)}
-    elif isinstance(threshold, dict):
-        threshold_dict = {q_col: threshold[q_col] for q_col in q_val_cols}
+    var_df = mdata[modality].var
+    mask = _mask_boolean_filter(
+        series_to_mask=var_df[column],
+        keep=keep,
+        value=value
+    )
+    
+    if "filter" not in mdata[modality].varm_keys():
+        mdata[modality].varm["filter"] = mask.to_frame(name=filter_name)
+    else:
+        mdata[modality].varm["filter"][filter_name] = mask
+    
+    if "filter" not in mdata[modality].uns_keys():
+        mdata[modality].uns["filter"] = {filter_name: (keep, value)}
+    else:
+        mdata[modality].uns["filter"][filter_name] = (keep, value)
 
-    q_values = adata.var[q_val_cols]
+    # add filter for decoy
+    if mstatus.__getattribute__(modality).has_decoy:
+        decoy_df = mdata[modality].uns["decoy"]
+        decoy_mask = _mask_boolean_filter(
+            series_to_mask=decoy_df[column],
+            keep=keep,
+            value=value
+        )
 
-    # Get filter result
-    filter_result = dict()
-    for q_col in q_val_cols:
-        filter_result[q_col] = q_values[q_col] < threshold_dict[q_col]
-        _save_filter(adata, q_col, filter_result[q_col], threshold_dict[q_col])
+        if "decoy_filter" not in mdata[modality].uns_keys():
+            mdata[modality].uns["decoy_filter"] = decoy_mask.to_frame(name=filter_name)
+        else:
+            mdata[modality].uns["decoy_filter"][filter_name] = decoy_mask
 
     return mdata
 
 
-@uns_logger
-def add_prefix_filter(
-    mdata: MuData,
-    prefix: str | tuple = ("rev_", "contam_"),
-) -> MuData:
-    mdata = mdata.copy()
-    adata = mdata["feature"]
-
-    # Get protein columns
-    proteins = _get_column(adata, "proteins", "search_result")
-
-    # Remove prefix matched from proteins column
-    pat = r"(^|;)\s*(?:" + "|".join(map(re.escape, prefix if isinstance(prefix, (tuple, list)) else (prefix,))) + r")"
-    # adata.var["proteins"] = proteins.apply(lambda x: _remove_prefix_matched(x, prefix))
-
-    # Get filter result
-    filter_result = ~proteins.str.contains(pat, regex=True, na=False)
-
-    # Save filter
-    _save_filter(adata, "prefix", filter_result, list(prefix))
-
-    return mdata
-
-
-@uns_logger
-def add_precursor_purity_filter(
-    mdata: MuData,
-    threshold: float,
-) -> MuData:
-    mdata = mdata.copy()
-    adata = mdata["feature"]
-
-    # Get filter result
-    filter_result = mdata["feature"].var["purity"] > threshold
-
-    # Save filter
-    _save_filter(adata, "purity", filter_result, threshold)
-
-    return mdata
-
-
-@uns_logger
-def add_decoy_filter(
-    mdata: MuData,
-) -> MuData:
-    mdata = mdata.copy()
-    adata = mdata["feature"]
-
-    decoy = _get_column(adata, "decoy", "search_result")
-    filter_result = decoy == 0
-
-    # Save filter
-    _save_filter(adata, "decoy", filter_result, 0)
-
-    return mdata
-
-
-@uns_logger
-def add_contaminant_filter(
-    mdata: MuData,
-) -> MuData:
-    mdata = mdata.copy()
-    adata = mdata["feature"]
-
-    contaminant = _get_column(adata, "contaminant", "search_result")
-    filter_result = contaminant == 0
-
-    # Save filter
-    _save_filter(adata, "contaminant", filter_result, 0)
-
-    return mdata
-
-
-@uns_logger
-def add_all_nan_filter(
-    mdata: MuData,
-    modality: str,
-) -> MuData:
-    mdata = mdata.copy()
-    adata = mdata[modality]
-
-    # Get filter result
-    filter_result = ~adata.to_df().isna().all(axis=0)
-
-    # Save filter
-    _save_filter(adata, "nan", filter_result, True)
-
-    return mdata
+def _mask_boolean_filter(series_to_mask:pd.Series, keep, value):
+    if keep == "eq":
+        return series_to_mask == value
+    elif keep == "ne":
+        return series_to_mask != value
+    elif keep == "lt":
+        return series_to_mask < value
+    elif keep == "le":
+        return series_to_mask <= value
+    elif keep == "gt":
+        return series_to_mask > value
+    elif keep == "ge":
+        return series_to_mask >= value
+    elif keep == "contains":
+        return series_to_mask.str.contains(str(value))
+    elif keep == "not_contains":
+        return ~series_to_mask.str.contains(str(value))
+    else:
+        raise ValueError(f"Unknown filter operator: {keep}")
 
 
 @uns_logger
 def apply_filter(
     mdata: MuData,
-    modality: str | list,
+    modality: str,
 ) -> MuData:
-    if isinstance(modality, str):
-        mods = [modality]
-    elif isinstance(modality, list):
-        mods = modality
-    else:
-        raise ValueError("modality should be a string or a list of strings")
+    """
+    Applies the filter to the specified modality in the MuData object.
 
-    # Apply filter
-    for mod in mods:
-        mdata = _apply_filter(mdata, mod)
+    Parameters:
+        mdata (MuData): MuData object to which the filter will be applied.
+        modality (str): The modality within the MuData object to which the filter will be applied.
+
+    Returns:
+        MuData: MuData object with the filter applied.
+    """
+    mdata=mdata.copy()
+    mstatus = MuDataStatus(mdata)
+
+    adata_to_filter = mdata[modality]
+    if "filter" not in adata_to_filter.varm_keys():
+        raise ValueError("No filter found in the modality's varm.")
+    
+    filtered_adata = adata_to_filter[:, adata_to_filter.varm["filter"].all(axis=1)].copy()
+    mdata.mod[modality] = filtered_adata
+
+    if mstatus.__getattribute__(modality).has_decoy:
+        decoy_df = adata_to_filter.uns["decoy"]
+        if "decoy_filter" not in adata_to_filter.uns_keys():
+            raise ValueError("No decoy filter found in the modality's uns.")
+        decoy_filter = adata_to_filter.uns["decoy_filter"]
+        decoy_filtered_df = decoy_df[decoy_filter.all(axis=1)].copy()
+        decoy_filter = decoy_filter.loc[decoy_filtered_df.index]
+
+        mdata[modality].uns["decoy"] = decoy_filtered_df
+        mdata[modality].uns["decoy_filter"] = decoy_filter
 
     return mdata.copy()
 
 
-def _apply_filter(
-    mdata: MuData,
-    modality: str,
-) -> MuData:
-
-    mdata = mdata.copy()
-
-    # Get filter result
-    if "filter" not in mdata[modality].varm_keys():
-        raise ValueError("Filter result is not found in the data")
-    filter_df = mdata[modality].varm["filter"]
-
-    mdata = subset(
-        mdata=mdata,
-        modality=modality,
-        cond_var=filter_df.all(axis=1),
-    )
-
-    return mdata
-
-
-def _get_column(
-    adata: AnnData,
-    colname: str,
-    key: str,
-) -> pd.Series:
-    # Check if the column is in the var or varm
-    if colname in adata.var.columns:
-        return adata.var[colname]
-    elif colname in adata.varm[key].columns:
-        return adata.varm[key][colname]
-    else:
-        raise ValueError(f"{colname} not found in the data")
-
-
-def _remove_prefix_matched(row: pd.Series, prefix: str | tuple) -> pd.Series:
-    matched_list = []
-    for x in row.split(";"):
-        if str(x).startswith(prefix):
-            continue
-        else:
-            matched_list.append(x)
-
-    matched_str = ";".join(matched_list)
-    if row == matched_str:
-        return row
-    else:
-        return ""
-
-
-def _save_filter(
-    adata: AnnData,
-    key: str,
-    filter_result: pd.Series,
-    content: str | list | tuple | dict,
-) -> None:
-    key = f"filter_{key}"
-
-    _save_filter_result(adata, key, filter_result)
-    _save_filter_content(adata, key, content)
-    return None
-
-
-def _save_filter_result(
-    adata: AnnData,
-    key: str,
-    filter_result: pd.Series,
-) -> None:
-    if "filter" not in adata.varm_keys():
-        adata.varm["filter"] = filter_result.to_frame(name=key)
-    else:
-        adata.varm["filter"][key] = filter_result
-    return None
-
-
-# TODO: make keeping ordered dict format
-def _save_filter_content(
-    adata: AnnData,
-    key: str,
-    content: dict,
-) -> None:
-    if "filter" not in adata.uns_keys():
-        adata.uns["filter"] = {key: content}
-    else:
-        adata.uns["filter"][key] = content
-    return None
+# def plot_filter_metric():
+    ...
