88a3969a7be11a3267d1685f4657f4c96d8ab17f
wook <hyungwook.choi@bertis.com>
2025-12-12 17:39:59 +0900

    feat: update module imports to include _read_write functions in __init__.py

--- msmu/__init__.py
+++ msmu/__init__.py
@@ -6,6 +6,7 @@ from .logging_utils import setup_logger, LogLevel
 from . import _plotting as pl
 from . import _preprocessing as pp
 from . import _tools as tl
+from . import _read_write as io
 from ._read_write._reader_utils import merge_mudata
 from ._read_write._reader_registry import read_h5mu, read_sage, read_diann, read_maxquant, read_fragpipe
 from . import _utils as utils
@@ -37,4 +38,5 @@ __all__ = [
     "pl",
     "tl",
     "utils",
+    "io",
 ]
