fix: ensure correct batch effect normalization by copying data before modification

msmu/_preprocessing/_normalise.py
@@ -143,7 +143,7 @@ def correct_batch_effect(
 
         gis_normalised_data: np.array[float] = _normalise_gis(arr=adata.X, gis_idx=gis_idx)
 
-        gis_drop_mod = adata[~gis_idx]
+        gis_drop_mod = adata[~gis_idx].copy()
         gis_drop_mod.X = gis_normalised_data
         mdata.mod[modality] = gis_drop_mod
 