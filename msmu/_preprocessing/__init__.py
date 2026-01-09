07c1535e811803117745e2b23a124c5b4050389a
jlthepi <jlthepi@gmail.com>
2026-01-09 18:03:41 +0900

    fix: add 'normalize' in import statements

--- msmu/_preprocessing/__init__.py
+++ msmu/_preprocessing/__init__.py
@@ -1,7 +1,7 @@
 from ._filter import add_filter, apply_filter
 from ._infer_protein import infer_protein
 from ._summarise import to_peptide, to_protein, to_ptm
-from ._normalise import correct_batch_effect, log2_transform, normalise, adjust_ptm_by_protein, scale_data
+from ._normalise import correct_batch_effect, log2_transform, normalise, normalize, adjust_ptm_by_protein, scale_data
 
 __all__ = [
     "add_filter",
